f60a1dd713ab75834cd27d41b69e9c0d8ff773a3
zuotian <zuotian@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-27 19:34:28 +0000

    tagging version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/enhanced-bibliplug@429528 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0/bibliplug_options.php
@@ -0,0 +1,130 @@
+<?php
+
+if (!current_user_can('manage_options'))
+	wp_die(__('You do not have sufficient permissions to manage options for this blog.'));
+
+$title = 'Bibliplug options';
+$parent_file = 'options-general.php';
+
+global $bib_query;
+
+if (isset($_POST['clear-reference']) && $_POST['clear-reference']) {
+	check_admin_referer('bibliplug_options');
+	$bib_query->create_data_tables(true);
+	unset($_POST['clear-reference']);
+}
+
+if (isset($_POST['refresh-schema']) && $_POST['refresh-schema']) {
+	check_admin_referer('bibliplug_options');
+	$bib_query->refresh_schema(true);
+	update_option('bibliplug_db_version', BIBLIPLUG_VERSION);
+	unset($_POST['refresh-schema']);
+}
+
+if (isset($_POST['upgrade-schema']) && $_POST['upgrade-schema']) {
+	check_admin_referer('bibliplug_options');
+	$bib_query->upgrade_schema();
+	update_option('bibliplug_db_version', BIBLIPLUG_VERSION);
+	unset($_POST['upgrade-schema']);
+}
+
+if (isset($_POST['update']) && $_POST['update']) {
+	check_admin_referer('bibliplug_options');
+	if (isset($_POST['page_size']) && $_POST['page_size']) {
+		update_option('bibliplug_page_size', $_POST['page_size']);
+	}
+	
+	if (isset($_POST['last_name_format']) && $_POST['last_name_format']) {
+		update_option('bibliplug_last_name_format', $_POST['last_name_format']);
+	}
+
+	if (isset($_POST['sync_setting']) && $_POST['sync_setting']) {
+		update_option('bibliplug_zotero_sync_setting', $_POST['sync_setting']);
+	}
+}
+
+$is_english = get_option('bibliplug_last_name_format') == 'english';
+$is_automatic = get_option('bibliplug_zotero_sync_setting') == 'automatic';
+?>
+
+<div class="wrap">
+	<?php screen_icon(); ?>
+	<h2><?php echo esc_html( $title ); ?></h2>
+
+	<form class="biblioplug_option" action="<? admin_url("admin.php?page=bibliplug/bibliplug_options"); ?>" method="post">
+		<?php wp_nonce_field('bibliplug_options'); ?>
+		<h3>Version</h3>
+		<p>
+			<?php printf('Your bibliplug database version is %s.', get_option('bibliplug_db_version')); ?>
+			<input type="submit" name="refresh-schema" class="button-primary schema" value="Refresh schema" />
+			<input type="submit" name="upgrade-schema" class="button-primary schema" value="Upgrade schema" />
+		</p>
+
+		<h3>Data size</h3>
+		<p>
+			<?php printf('Your bibliplug database has %d references.  ', $bib_query->get_rows()); ?>
+			<input type="submit" name="clear-reference" class="button-primary schema" value="Delete references" />
+		</p>
+
+		<h3>Options</h3>
+		<p>The number of references shown on the admin edit page.
+			<input class="text" name="page_size" value="<?php echo get_option('bibliplug_page_size'); ?>" />
+		</p>
+
+		<p>Last name display format:
+			<input type="radio" name="last_name_format" value="english" <?php echo $is_english ? 'checked' : ''; ?> /> English
+			<input type="radio" name="last_name_format" value="dutch" <?php echo $is_english ? '' : 'checked'; ?>/> Dutch
+		</p>
+		<p>&nbsp;</p>
+		<p>Update scheduling:
+			<select name="sync_setting">
+				<option value="automatic" <?php echo $is_automatic ? 'selected' : ''; ?> >Automatic</option>
+				<option value="manual" <?php echo $is_automatic ? '' : 'selected'; ?> >Manual</option>
+			</select>
+		</p>
+		<span class="description">
+			<p>You need to use a cron job for automatic synchronization. For example, inserting the following line in your crontab:</p>
+			<pre>	<code>*/30 * * * * /usr/local/bin/curl --silent <?php echo get_bloginfo('url') . '?bibliplug_sync_zotero=1'; ?></code></pre>
+			<p>will send requests to your site every 30 minutes and check for updates on all zotero connections.</p>
+		</span>
+
+		<div class="clear"><br></div>
+		<p>
+			<input type="submit" name="update" class="button-primary" value="Update options" />
+		</p>
+
+	</form>
+<?php
+/*
+// Print out fields for each bib type.
+global $wpdb;
+  
+$type_names = $bib_query->get_types();
+foreach($type_names as $type) {
+	echo '<h3>'.$type->name.'</h3>';
+	echo "<table class='form_table' border='1'>";
+	$creator_names = $bib_query->get_creator_types($type->id);
+ 	foreach ($creator_names as $creator) {
+		echo '<tr>';
+ 		echo "<td>".$creator->name.'</td>';
+ 		echo '</tr>';
+ 	}
+	
+	
+
+	$field_names = $bib_query->get_fields_by_type_id($type->id); 
+ 	foreach ($field_names as $field) {
+ 		echo '<tr>';
+ 		echo "<td>".$field->name.'</td>';
+  		if ($mapped_name) {
+  			echo '<td>  =>  '.$mapped_name.'</td>'; 
+  		}
+  		else {
+  			echo '<td></td>';
+  		}
+ 		echo '</tr>';
+ 	}
+ 	echo '</table>';
+}
+*/
+?>
---
+++ tags/1.0/bibliplug_template.php
@@ -0,0 +1,146 @@
+<?php
+
+class bibliplug_template {
+	
+	public function print_creator_list($creators, $creator_types)
+	{
+		?><table id="bibliplug-creators">
+			<thead>
+				<tr>
+					<th>Type</th>
+					<th>First Name</th>
+					<th>Middle Name</th>
+					<th>Prefix</th>
+					<th>Last Name</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody><?php 
+			if (!$creators)
+			{
+				// creator an empty place holder.
+				$creators = array((object) array('id' => 0, 'order_index' => 0));
+			}
+			
+			foreach ($creators as $creator)  { 
+				?><tr id="creator-row-<?php echo $creator->order_index; ?>">
+					<input type="hidden" class="order-index" name="creator[<?php echo $creator->id; ?>][order_index]" value="<?php echo $creator->order_index; ?>" />
+					<td>
+						<select name="creator[<?php echo $creator->id; ?>][creator_type_id]"><?php
+							foreach ($creator_types as $type) {
+								$attr = ($type->id == $creator->creator_type_id) ? 'selected="selected"' : '';
+								?><option value="<?php echo $type->id; ?>" <?php echo $attr; ?>><?php echo ucfirst($type->name); ?></option><?php 
+							}
+						?></select>
+					</td>
+					<td>
+						<input type="text" name="creator[<?php echo $creator->id; ?>][first_name]" value="<?php echo $creator->first_name; ?>" />
+					</td>
+					<td>
+						<input type="text" name="creator[<?php echo $creator->id; ?>][middle_name]" value="<?php echo $creator->middle_name; ?>" />
+					</td>
+					<td>
+						<input type="text" name="creator[<?php echo $creator->id; ?>][prefix]" value="<?php echo $creator->prefix; ?>" />
+					</td>
+					<td>
+						<input type="text" name="creator[<?php echo $creator->id; ?>][last_name]" value="<?php echo $creator->last_name; ?>" />
+					</td>
+					<td>
+						<span class="delete-creator"><img src="<?php echo admin_url('images/no.png'); ?>" alt="x" /></span>
+					</td>
+				</tr><?php 
+			}
+			?></tbody>
+		</table>
+		<p><input type="button" class="button" id="add-creator" value="add-creator" /></p>
+		<p class="description">Drag and drop creators to change the order.</p>
+		<?php
+		
+	}
+	
+	public function print_details_table($bib, $type_id, $fields) {
+		global $bib_query;
+	
+		?><table class="form-table" id="bibliplug-details">
+			<tr valign="top">
+				<th scope="row"><label for="type_id">Type</label></th>
+				<td><select id ="bibliplug_ref_type"name="type_id" >
+				<?php foreach ($bib_query->get_types() as $type) {
+					$attr = ($type->id == $type_id) ? '" selected="selected' : '';
+					?><option value="<?php echo $type->id.$attr; ?>"><?php echo ucfirst($type->name); ?></option><?php
+				} ?>
+				</select></td>
+			</tr>
+			
+			<?
+			if ($fields) {
+				foreach ($fields as $field) {
+					$field_name = $field->internal_name;
+					$field_display_name = ($field->mapped_name) ? $field->mapped_name : $field->name;
+					$field_value = stripslashes($bib->$field_name); 
+			?>
+			<tr valign="top">
+				<th scope="row"><label for="<? echo $field_name; ?>"><? echo ucfirst($field_display_name); ?></label></th>
+				<td colspan="3">
+				<? switch ($field_name) {
+					case 'title':
+						?><textarea name="<? echo $field_name; ?>" rows="4" id="<? echo $field_name; ?>" class='textarea'><? echo esc_attr($field_value); ?></textarea><?
+						break;
+					case 'abstract':
+					case 'notes':
+						?><textarea name="<? echo $field_name; ?>" rows="4" id="<? echo $field_name; ?>" class='textarea'><? echo esc_attr($field_value); ?></textarea><?
+						break;
+					
+					case 'start_page':
+					case 'end_page':
+					case 'volume':
+					case 'number_of_volumes':
+						?><input name="<? echo $field_name; ?>" type="text" id="<? echo $field_name; ?>" value="<? echo $field_value ? esc_attr($field_value) : ''; ?>" class="short"/><?
+						break;
+					
+					case 'publish_year':
+					case 'series':
+					case 'edition':
+					case 'ISBN':
+					case 'ISSN':
+					case 'publish_date':
+					case 'access_date':
+					case 'volume':
+					case 'issue':
+						?><input name="<? echo $field_name; ?>" type="text" id="<? echo $field_name; ?>" value="<? echo esc_attr($field_value); ?>" class="short"/><?
+						break;
+					
+					case 'keywords':
+						$hint = 'separate multiple keywords with commas';
+						?><input name="<? echo $field_name; ?>" type="text" id="<? echo $field_name; ?>" value="<? echo esc_attr($field_value); ?>" class="input-with-hint long"/><span class="bibliplug-hint"><? echo $hint; ?></span><?
+						break;
+					case 'link1':
+						$hint = 'link full text document to reference title';
+						?><input name="<? echo $field_name; ?>" type="text" id="<? echo $field_name; ?>" value="<? echo esc_attr($field_value); ?>" class="input-with-hint long"/><span class="bibliplug-hint"><? echo $hint; ?></span><?
+						break;
+					case 'link2':
+						$hint = 'link full text html webpage to title';
+						?><input name="<? echo $field_name; ?>" type="text" id="<? echo $field_name; ?>" value="<? echo esc_attr($field_value); ?>" class="input-with-hint long"/><span class="bibliplug-hint"><? echo $hint; ?></span><?
+						break;
+					case 'link3':
+						$hint = 'link source/information page to secondary title';
+						?><input name="<? echo $field_name; ?>" type="text" id="<? echo $field_name; ?>" value="<? echo esc_attr($field_value); ?>" class="input-with-hint long"/><span class="bibliplug-hint"><? echo $hint; ?></span><?
+						break;
+					
+					case 'url':
+						$hint = 'enter link here to make URL visible in reference';
+						?><input name="<? echo $field_name; ?>" type="text" id="<? echo $field_name; ?>" value="<? echo esc_attr($field_value); ?>" class="input-with-hint long"/><span class="bibliplug-hint"><? echo $hint; ?></span><?
+						break;
+					
+					default:
+						?><input name="<? echo $field_name; ?>" type="text" id="<? echo $field_name; ?>" value="<? echo esc_attr($field_value); ?>" class="long"/><?
+						break;
+				} ?>
+				</td>
+			</tr>
+		<? }
+		} ?>
+		</table><?
+	}
+}
+?>
